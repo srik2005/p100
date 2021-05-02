99826d3af9d0976f47b3655683117893ee87db90
srik2005 <62752134+srik2005@users.noreply.github.com>
2021-05-02 20:05:02 +0530

    Add files via upload

---
+++ atm.py
@@ -0,0 +1,23 @@
+class Atm:
+    def __init__(self):
+        self.balance = 0
+        print("The account is created")
+        
+
+    def deposit(self):
+        amount = int(input("Enter the amount to be deposited: "))
+        self.balance = self.balance + amount
+        print ("Deposit is successful and the balance is %f" % self.balance)
+
+    def withdraw(self):
+        amount = int(input("Enter the amount for the withdrawal: "))
+        if (self.balance >= amount):
+            self.balance = self.balance - amount
+            print("Withdrawal is Successfull and the balance is %f " % self.balance)
+
+        else:
+            print("Insuffient Balance!!")
+        
+acc = Atm()
+acc.deposit()
+acc.withdraw()
